ae782f15b463fecc99e62f2a3b178095c7882012
tree105778 <bjyon0415@gmail.com>
2025-05-12 09:10:18 +0900

    chat-service 라이브러리 추가 및 설정 추가

--- chat-service/src/main/java/com/playdata/chatservice/controller/ChatController.java
+++ chat-service/src/main/java/com/playdata/chatservice/controller/ChatController.java
@@ -1,10 +1,8 @@
 package com.playdata.chatservice.controller;
 
 import com.playdata.chatservice.dto.ChatMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
